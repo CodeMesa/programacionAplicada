9d63a53950ea7bab45c70c76f1306e674a9dc496
MesaJG <142937447+MesaJG@users.noreply.github.com>
2023-09-12 15:21:09 -0500

    Update listas.py

--- programas/segundoCorte/EstructurasDeDatos/listas.py
+++ programas/segundoCorte/EstructurasDeDatos/listas.py
@@ -1,1 +1,51 @@
+my_lista = ['Rojo', 'Azul', 'Amarillo', 'Naranja', 'Violeta', 'Verde']
+
+print(my_lista)
+print(type(my_lista))
+print(my_lista[2])
+
+print("my_lista size: ", len(my_lista))
+print(my_lista[0:2])
+print(my_lista[:2])
+
+my_lista.append('Blanco')      #Agrega elemento al final de la lista
+print(my_lista)
+
+my_lista.insert(3, 'Negro')
+print(my_lista)
+
+my_lista.extend(['Marron', 'Gris'])   #Concatena a otra lista
+print(my_lista)
+
+print(my_lista.index('Azul')) #busca el num 
+
+my_lista.remove('Marron') #quita 
+print(my_lista)
+
+my_lista.insert(8, 'Marron') #pone
+print(my_lista)
+
+print(my_lista.pop())          
+size = len(my_lista)
+print("size = ", size)
+#print(my_lista.pop(size))
+
+my_lista_3 = my_lista*3
+print("my_lista_3: ", my_lista_3)
+
+print("Sort:")
+print()
+my_listaSort = my_lista.sort()
+print(my_listaSort)
+
+my_NumList = [10, 9, 8, 7, 6 , 5 , 4, 3, 2, 1]
+print("Ordering my_NumList: ")
+my_NumList.sort()
+print(my_NumList)
+#OrderedLList = my_NumList.sort()
+#print(my_listaSort)
+
+#Ordenando lista de mayor a menor
+my_NumList.sort(reverse = True)
+print("De menor a mayor: ", my_NumList)
 
