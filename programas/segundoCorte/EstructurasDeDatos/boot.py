1609bdcdfb064a2ca8f47256fc6d27efba4cbe08
MesaJG <142937447+MesaJG@users.noreply.github.com>
2023-10-30 23:48:37 -0500

    Update boot.py

--- programas/segundoCorte/EstructurasDeDatos/boot.py
+++ programas/segundoCorte/EstructurasDeDatos/boot.py
@@ -1,10 +1,33 @@
-import time 
-from machine import Pin 
-led = Pin(2,Pin.OUT)
-b=0
-while b<5 :
-  led.value(0.2)
-  time.sleep(1)
-  led.value(0)
-  time.sleep(0.1)
-  b=b+1
+import network
+import urequests
+from machine import ADC
+import time
+
+# Configuracion Wi-Fi
+ssid = "ROSCAPUMPA"
+password = "tommy2209"
+
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+wifi.connect(ssid, password)
+
+while not wifi.isconnected():
+    pass
+
+print("Conexión exitosa. IP: " + wifi.ifconfig()[0])
+
+potentiometer_pin = ADC(34)
+
+thingspeak_url = "https://api.thingspeak.com/update?api_key=D22T9B5Q3OCKO9HL&field1="
+
+while True:
+
+    potentiometer_value = potentiometer_pin.read()
+
+    print("Valor del potenciómetro:", potentiometer_value)
+
+    url = thingspeak_url + str(potentiometer_value)
+    response = urequests.get(url)
+    print("Respuesta de ThingSpeak:", response.text)
+    response.close()
+    time.sleep(15)
